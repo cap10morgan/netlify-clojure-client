ba51e4b085a5de7a11d14b1d1186544ba0b34000
Wes Morgan <wesmorgan@icloud.com>
2018-02-20 15:11:20 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cap10morgan/netlify-client "0.1.0"
+(defproject cap10morgan/netlify-client "0.1.1-SNAPSHOT"
   :description "Netlify API Client"
   :url "https://github.com/cap10morgan/netlify-client"
   :license {:name "Eclipse Public License"
