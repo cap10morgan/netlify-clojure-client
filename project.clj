00eccc032792fd49d69f1960a4abaf13accd7204
Wes Morgan <wesmorgan@icloud.com>
2018-05-10 14:59:48 -0600

    Update clj-http to 3.9.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/spec.alpha "0.1.143"]
-                 [clj-http "3.8.0"]
+                 [clj-http "3.9.0"]
                  [cheshire "5.8.0"]
                  [plumula/mimolette "0.2.1"]
                  [com.cemerick/url "0.1.1"]
